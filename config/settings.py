10b944cf1ba0be055b4f1070d03dbd97d7763a24
Moynul <moynul.officials@gmail.com>
2025-07-31 19:00:30 +0600

    final

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ DEBUG = env.bool('DEBUG', default=False)
 if DEBUG:
     BASE_DIR = Path(__file__).resolve().parent.parent
 else:
-    BASE_DIR = Path('/home/routines/www/routines')
+    BASE_DIR = Path('/home/routine/www/routines')
 
 
 DEBUG = env.bool('DEBUG', default=False)
