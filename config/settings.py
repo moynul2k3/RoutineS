f8f41dd0cabb61f0e00cc2616c4a798627116aef
Moynul <moynul.officials@gmail.com>
2025-07-31 19:21:12 +0600

    final

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django_extensions',
+    'django_extensions',
 
     'tailwind',
     'theme',
